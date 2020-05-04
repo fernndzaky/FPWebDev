bcb95aadda32d4c24db016befeec9c7abaae72b9
Fernandha Dzaky <fernandhadzaky@hotmail.com>
2020-05-04 22:55:05 +0700

    rapihin musician dashboard

--- resources/views/musician-dashboard/musicianpage.blade.php
+++ resources/views/musician-dashboard/musicianpage.blade.php
@@ -1,15 +1,13 @@
 @extends('layout/musician')
-
 @section('title','MINDER')
-
 @section('container')
 <!-- START MUSICIAN PAGE -->
 <div class="container">
     <div class="row m-0" style="padding-top:30px">
         <div class="col-sm-4 pl-0">
-        <img src="assets/nugie 1.png" style="width:100%"alt="">
+            <img src="assets/nugie 1.png" style="width:100%"alt="">
         </div>
-        <div class="col-sm-8 pl-0">
+        <div class="col-sm-8 pl-4">
             <div style="text-align:center">
                 <h1 style="color:#0883CC;font-family:quicksand;font-weight:bold;font-size:50px">
                     MUSICIAN NAME
@@ -20,39 +18,37 @@
             </div>
             <div style = "text-align:left">
                 <p style="font-family: Quicksand;font-weight: bold;font-size: 24px;color: #61BDDC;padding-top:10px">
-                Musician Description :
+                    Musician Description :
                 </p>
-                <p style="font-family: Quicksand;font-weight: bold;font-size: 18px;color: #61BDDC" >Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
+                <p style="font-family: Quicksand;font-weight: bold;font-size: 18px;color: #61BDDC" >
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
             </div>
                 <div class="row p-0 m-0" style="padding-top:80px">
                     <div class="col-sm-6 pl-0" style="padding-top:10px">
-                        <h1 style="font-family: Quicksand;font-weight: bold;font-size: 24px;color: #61BDDC">
+                        <h2 style="font-family: Quicksand;font-weight: bold;font-size: 24px;color: #61BDDC">
                             STATUS : <br> Currently searhing for a Band
-                        </h1>
-                    
+                        </h2>    
+                    </div>
+                    <div class="col-sm-6">
+                        <button class="btn-blue" style="border-radius:50px;font-family: Quicksand"> CHANGE STATUS </button><br><br>
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    <button class="btn-blue" style="border-radius:50px;font-family: Quicksand"> CHANGE STATUS </button><br><br>
-                </div>
-            </div>
         </div>
-                
-        
     </div>
 </div>
 <!-- END MUSICIAN PAGE -->
- <!-- parallax -->
- <div style="padding-top:50px">
- <div class="parallax" style="text-align: center;padding:80px 0px 40px 0px">
-      <h1 style="color: white;font-size: 70px">IT’S SO MUCH MORE THAN <br> JUST A BAND</h1>
-    </div>
-    </div>
-  <!-- end of parallax -->
 
-  <!-- start content 2  -->
-  <div class="row m-0" style="padding: 80px 0px">
+<!-- PARALLAX -->
+<div style="padding-top:50px">
+    <div class="parallax" style="text-align: center;padding:80px 0px 40px 0px">
+        <h1 style="color: white;font-size: 70px">IT’S SO MUCH MORE THAN <br> JUST A BAND</h1>
+    </div>
+</div>
+<!-- END OF PARALLAX -->
+
+<!-- START CONTENT 2  -->
+<div class="row m-0" style="padding: 80px 0px">
     <div class="col-sm-6" style="text-align:center;">
       <h1 style="color:#2EA8D1;font-family:quicksand;font-weight:bold;padding:120px 50px 0px 50px"> ARE YOU A SOLO MUSICIAN? FIND YOUR BAND NOW! </h1>
       <button class="btn-blue" style="margin-top:5%">FIND BAND</button>
@@ -60,9 +56,6 @@
     <div class="col-sm-6 wow fadeInRight" style="text-align:center" >
       <img src="assets/bassist 1.png" alt="">
     </div>
-  </div>
-  <!-- end content 2 -->
-
- 
-
+</div>
+<!-- END CONTENT 2 -->
 @endsection
